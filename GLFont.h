3ac077eecaeb68e62cdf17b46eb5ac3410c26af8
Pan <pan@spinningkids.org>
2021-04-05 11:29:55 +0200

    Cleanup and reformat GLFont

--- GLFont.h
+++ GLFont.h
@@ -12,32 +12,29 @@
 #ifdef WIN32
 #define NOMINMAX
 #include <Windows.h>
-#include <GL/gl.h>
+#include <gl/GL.h>
 #else
-#include <GL/gl.h>
+#include <gl/GL.h>
 #include "glf.h"
 #endif
 
 class GLFont {
-  GLYPHMETRICSFLOAT gmf[96];
-  GLuint base;
+    GLYPHMETRICSFLOAT gmf[96];
+    GLuint base;
 public:
 #ifdef WIN32
-  GLFont(HDC hDC, const char *s);
+    GLFont(HDC hDC, const char* s);
 #else
-  GLFont(GLF_MEMFILE *font);
+    GLFont(GLF_MEMFILE* font);
 #endif
 
-  void printf(const char *fmt, ...) const;
-  void printfx(float x, float y, float gs, float gr, float lsx, float lsy, float lr, float tracking, const char *fmt, ...) const;
+    void printChar(char c, float lsx, float lsy, float lr, float tracking) const;
 
-  void printChar(char c, float lsx, float lsy, float lr, float tracking) const;
+    float getWidth(char c, float tracking = 0) const { return gmf[c - 32].gmfCellIncX * (tracking + 1); }
+    float getHeight(char c) const { return gmf[c - 32].gmfBlackBoxY; }
+    void skipChar(char c, float tracking = 0) const { glTranslatef(getWidth(c, tracking), 0, 0); }
 
-  float getWidth(char c, float tracking = 0) const { return gmf[c-32].gmfCellIncX*(tracking+1); }
-  float getHeight(char c) const { return gmf[c-32].gmfBlackBoxY; }
-  void skipChar(char c, float tracking = 0) const { glTranslatef(getWidth(c, tracking), 0, 0); }
-
-  ~GLFont();
+    ~GLFont();
 };
 
 #endif // GLFONT_H_
